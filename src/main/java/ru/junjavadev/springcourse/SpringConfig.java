d5603c13884939a5c292b3cd392ea091f7ff8439
Alexander Ogarkov <ogarkov.pro@gmail.com>
2023-03-27 03:24:39 +0300

    lesson Spring Core 13 config in java class

---
+++ src/main/java/ru/junjavadev/springcourse/SpringConfig.java
@@ -0,0 +1,11 @@
+package ru.junjavadev.springcourse;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ru.junjavadev.springcourse")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+}
--- src/main/java/ru/junjavadev/springcourse/TestSpring.java
+++ src/main/java/ru/junjavadev/springcourse/TestSpring.java
@@ -1,14 +1,16 @@
 package ru.junjavadev.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class TestSpring {
     public static void main(String[ ] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                "applicationContext.xml"
+//        );
 //        Computer computer = context.getBean("computer", Computer.class);
 //        System.out.println(computer);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
